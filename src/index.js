66ecf4c459d8a28ad12fb1ea93866334602f7c7d
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-03 19:56:49 +0500

    feat: add getFileContents

--- src/index.js
+++ src/index.js
@@ -2,25 +2,38 @@ import { cwd } from 'node:process';
 import path from 'node:path';
 import { readFileSync } from 'node:fs';
 
-
-
-
 const readFile = (filePath) => {
   const currDir = cwd();
-  return readFileSync(path.resolve(currDir, filePath));
+  const fileOfPath = path.resolve(currDir, filePath);
+  return readFileSync(fileOfPath);
 }
 
-const getExtName = (file) => path.extname(file);
+const getExtName = (filePath) => path.extname(filePath);
+
+const getFileContents = (file) => {
+  let fileContents = '';
+  try {
+    fileContents = JSON.parse(readFile(file));
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      fileContents = null;
+      console.log(`File ${file} not found!`);
+    } else {
+      throw err;
+    }
+  }
+  return fileContents;
+};
 
 const genDiff = (file1, file2) => {
   const extname1 = getExtName(file1);
   const extname2 = getExtName(file2);
+  let file1Contents = getFileContents(file1);
 
 
-  // let student = JSON.parse(rawd);
 
 
-  return extname1;
+
 }
 
-console.log(genDiff('../assets/file1.json', '../assets/file2.json'));
+console.log(genDiff('/assets/file1.json', '../assets/file2.json'));
