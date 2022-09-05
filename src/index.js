e6e9c33f0966462b0d409f8d63e64d0126575364
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-05 15:54:33 +0500

    fix: refactor diffJson and genDiff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { cwd } from "node:process";
-import path from "node:path";
-import { readFileSync } from "node:fs";
-import _ from "lodash";
+import { cwd } from 'node:process';
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+import _ from 'lodash';
 
 const readFile = (filePath) => {
   const currDir = cwd();
@@ -12,11 +12,11 @@ const readFile = (filePath) => {
 const getExtName = (filePath) => path.extname(filePath);
 
 const getFileContents = (file) => {
-  let fileContents = "";
+  let fileContents = '';
   try {
     fileContents = JSON.parse(readFile(file));
   } catch (err) {
-    if (err.code === "ENOENT") {
+    if (err.code === 'ENOENT') {
       fileContents = null;
       console.log(`File ${file} not found!`);
     } else {
@@ -34,42 +34,45 @@ const getSortedUnionKeys = (obj1, obj2) => {
 
 const diffJSON = (obj1, obj2) => {
   const allSortedKeys = getSortedUnionKeys(obj1, obj2);
-  let result = `\n`;
-  for (const key of allSortedKeys) {
+  const result = allSortedKeys.reduce((acc, key) => {
+    let str = acc;
     if (
       Object.hasOwn(obj1, key) &&
       Object.hasOwn(obj2, key) &&
       obj1[key] === obj2[key]
     ) {
-      result = `${result}   ${key}: ${obj1[key]}\n`;
+      str += `   ${key}: ${obj1[key]}\n`;
     } else if (
       Object.hasOwn(obj1, key) &&
       Object.hasOwn(obj2, key) &&
       obj1[key] !== obj2[key]
     ) {
-      result = `${result} - ${key}: ${obj1[key]}\n`;
-      result = `${result} + ${key}: ${obj2[key]}\n`;
+      str += ` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
     } else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      result = `${result} - ${key}: ${obj1[key]}\n`;
+      str += ` - ${key}: ${obj1[key]}\n`;
     } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      result = `${result} + ${key}: ${obj2[key]}\n`;
+      str += ` + ${key}: ${obj2[key]}\n`;
     }
-  }
+    return str;
+  }, `\n`);
+
   return `{${result}}`;
 };
 
 const genDiff = (file1, file2) => {
   const extname1 = getExtName(file1);
   const extname2 = getExtName(file2);
-  let file1Contents = getFileContents(file1);
-  let file2Contents = getFileContents(file2);
+  const file1Contents = getFileContents(file1);
+  const file2Contents = getFileContents(file2);
+  let result = '';
   if (extname1 === extname2 && file1Contents && file2Contents) {
-    return diffJSON(file1Contents, file2Contents);
+    result = diffJSON(file1Contents, file2Contents);
   } else if (extname1 !== extname2) {
-    return "Extnames are not equal.";
+    result = 'Extnames are not equal.';
   } else {
-    return "One of files is empty";
+    result = 'One of files is empty';
   }
+  return result;
 };
 
 export default genDiff;
