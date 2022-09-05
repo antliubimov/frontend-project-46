9378c59cef7aa7fa6d0ac0d983112336e5da3b06
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-05 15:53:56 +0500

    lint: gendiff single quotes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import genDiff from "../src/index.js";
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
-  .name("gendiff")
-  .description("Compares two configuration files and shows a difference.")
-  .version("1.0.0")
-  .arguments("<filepath1> <filepath2>")
-  .option("-f, --format <type>", "output format")
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
 program.parse();
