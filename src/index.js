9ce84602a593b473e7f1682544b57b3e0dae132e
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-15 08:33:02 +0000

    fix: change diffObjects

--- src/index.js
+++ src/index.js
@@ -31,34 +31,70 @@ const getFileContent = (file, extname) => {
 const getSortedUnionKeys = (obj1, obj2) => {
   const obj1Keys = _.keys(obj1);
   const obj2Keys = _.keys(obj2);
-  return _.sortedUniq([...obj1Keys, ...obj2Keys].sort());
+  return _.sortBy(_.union(obj1Keys, obj2Keys));
+};
+
+const isObject = (obj) => {
+  return Object.prototype.toString.call(obj) === '[object Object]';
 };
 
 const diffObjects = (obj1, obj2) => {
   const allSortedKeys = getSortedUnionKeys(obj1, obj2);
-  const result = allSortedKeys.reduce((acc, key) => {
-    let str = acc;
-    if (
-      Object.hasOwn(obj1, key) &&
-      Object.hasOwn(obj2, key) &&
-      obj1[key] === obj2[key]
-    ) {
-      str += `   ${key}: ${obj1[key]}\n`;
-    } else if (
-      Object.hasOwn(obj1, key) &&
-      Object.hasOwn(obj2, key) &&
-      obj1[key] !== obj2[key]
-    ) {
-      str += ` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
-    } else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      str += ` - ${key}: ${obj1[key]}\n`;
-    } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      str += ` + ${key}: ${obj2[key]}\n`;
-    }
-    return str;
-  }, `\n`);
+  // const result = allSortedKeys.reduce((acc, key) => {
+  //   let str = acc;
+  //   if (
+  //     Object.hasOwn(obj1, key) &&
+  //     Object.hasOwn(obj2, key) &&
+  //     obj1[key] === obj2[key]
+  //   ) {
+  //     str += `   ${key}: ${obj1[key]}\n`;
+  //   } else if (
+  //     Object.hasOwn(obj1, key) &&
+  //     Object.hasOwn(obj2, key) &&
+  //     obj1[key] !== obj2[key]
+  //   ) {
+  //     str += ` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
+  //   } else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+  //     str += ` - ${key}: ${obj1[key]}\n`;
+  //   } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+  //     str += ` + ${key}: ${obj2[key]}\n`;
+  //   }
+  //   return str;
+  // }, `\n`);
 
-  return `{${result}}`;
+  return allSortedKeys.map((key) => {
+    if (isObject(obj1[key]) && isObject(obj2[key])) {
+      return {
+        type: 'nested',
+        key,
+        value: diffObjects(obj1[key], obj2[key]),
+      };
+    } else if (!_.has(obj1, key)) {
+      return {
+        type: 'added',
+        key,
+        value: obj2[key],
+      };
+    } else if (!_.has(obj2, key)) {
+      return {
+        type: 'deleted',
+        key,
+        value: obj1[key],
+      };
+    } else if (!_.isEqual(obj1[key], obj2[key])) {
+      return {
+        type: 'changed',
+        key,
+        value1: obj1[key],
+        value2: obj2[key],
+      };
+    }
+    return {
+      type: 'unchanged',
+      key,
+      value: obj1[key],
+    };
+  });
 };
 
 const genDiff = (file1, file2) => {
@@ -76,5 +112,9 @@ const genDiff = (file1, file2) => {
   }
   return result;
 };
+
+console.log(
+  genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')
+);
 
 export default genDiff;
