fed6acf82969800510ce1f465f3733d9299816c4
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-17 15:59:38 +0000

    feat: make stylish

--- src/index.js
+++ src/index.js
@@ -34,109 +34,100 @@ const getSortedUnionKeys = (obj1, obj2) => {
   return _.sortBy(_.union(obj1Keys, obj2Keys));
 };
 
-const isObject = (obj) => {
-  return Object.prototype.toString.call(obj) === '[object Object]';
-};
+const isObject = (obj) =>
+  Object.prototype.toString.call(obj) === '[object Object]';
 
 const diffObjects = (obj1, obj2) => {
   const allSortedKeys = getSortedUnionKeys(obj1, obj2);
-  // const result = allSortedKeys.reduce((acc, key) => {
-  //   let str = acc;
-  //   if (
-  //     Object.hasOwn(obj1, key) &&
-  //     Object.hasOwn(obj2, key) &&
-  //     obj1[key] === obj2[key]
-  //   ) {
-  //     str += `   ${key}: ${obj1[key]}\n`;
-  //   } else if (
-  //     Object.hasOwn(obj1, key) &&
-  //     Object.hasOwn(obj2, key) &&
-  //     obj1[key] !== obj2[key]
-  //   ) {
-  //     str += ` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
-  //   } else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-  //     str += ` - ${key}: ${obj1[key]}\n`;
-  //   } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-  //     str += ` + ${key}: ${obj2[key]}\n`;
-  //   }
-  //   return str;
-  // }, `\n`);
 
   return allSortedKeys.map((key) => {
+    let result;
     if (isObject(obj1[key]) && isObject(obj2[key])) {
-      return {
+      result = {
         type: 'nested',
         key,
         value: diffObjects(obj1[key], obj2[key]),
       };
     } else if (!_.has(obj1, key)) {
-      return {
+      result = {
         type: 'added',
         key,
         value: obj2[key],
       };
     } else if (!_.has(obj2, key)) {
-      return {
+      result = {
         type: 'deleted',
         key,
         value: obj1[key],
       };
     } else if (!_.isEqual(obj1[key], obj2[key])) {
-      return {
+      result = {
         type: 'changed',
         key,
         value: [obj1[key], obj2[key]],
       };
+    } else {
+      result = {
+        type: 'unchanged',
+        key,
+        value: obj1[key],
+      };
     }
-    return {
-      type: 'unchanged',
-      key,
-      value: obj1[key],
-    };
+    return result;
   });
 };
 
 const makeReplaces = (depth, replacer = ' ', spacesCount = 2) =>
   `${replacer.repeat(spacesCount * (2 * depth - 1))}`;
 
-const getString = (key, value, depth, symbol = ' ') =>
-  `${makeReplaces(depth)}${symbol} ${key}: ${makeString(value, depth)}`;
-
 const makeString = (data, depth) => {
   if (!isObject(data)) {
     return data;
   }
 
-  const result = Object.entries(data).map(([key, value]) =>
-    getString(key, value, depth + 1)
+  const result = Object.entries(data).map(
+    ([key, value]) =>
+      `${makeReplaces(depth + 1)}  ${key}: ${makeString(value, depth + 1)}`
   );
   return `{\n${result.join('\n')}\n${makeReplaces(depth)}  }`;
 };
 
+const getString = (key, value, depth, symbol = ' ') =>
+  `${makeReplaces(depth)}${symbol} ${key}: ${makeString(value, depth)}`;
+
 const stylish = (data) => {
-  const iter = (data, depth) =>
-    data.map(({ type, key, value }) => {
+  const iter = (node, depth) =>
+    node.map(({ type, key, value }) => {
+      let result;
       switch (type) {
         case 'nested':
-          return [
+          result = [
             `${makeReplaces(depth)}  ${key}: {`,
             iter(value, depth + 1),
             `${makeReplaces(depth)}  }`,
           ];
+          break;
         case 'added':
-          return getString(key, value, depth, '+');
+          result = getString(key, value, depth, '+');
+          break;
         case 'deleted':
-          return getString(key, value, depth, '-');
+          result = getString(key, value, depth, '-');
+          break;
         case 'changed':
-          return `${getString(key, value[0], depth, '-')}\n${getString(
+          result = `${getString(key, value[0], depth, '-')}\n${getString(
             key,
             value[1],
             depth,
             '+'
           )}`;
+          break;
         case 'unchanged':
-          return getString(key, value, depth);
+          result = getString(key, value, depth);
+          break;
+        default:
+          throw new Error(`This ${type} type is not found`);
       }
+      return result;
     });
 
   const result = iter(data, 1).flat(Infinity);
@@ -151,7 +142,9 @@ const genDiff = (file1, file2, formatter = 'stylish') => {
   let result = '';
   if (extname1 === extname2 && file1Content && file2Content) {
     const tree = diffObjects(file1Content, file2Content);
-    result = stylish(tree);
+    if (formatter === 'stylish') {
+      result = stylish(tree);
+    }
   } else if (extname1 !== extname2) {
     result = 'Extnames are not equal';
   } else {
@@ -159,9 +152,5 @@ const genDiff = (file1, file2, formatter = 'stylish') => {
   }
   return result;
 };
-
-// console.log(
-//   genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json')
-// );
 
 export default genDiff;
