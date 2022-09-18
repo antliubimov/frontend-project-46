1f400625bc90491e2ab8353c7101ecdf68fcd71a
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-18 22:30:23 +0500

    fix: add formatter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import path from 'node:path';
 import { readFileSync } from 'node:fs';
 import _ from 'lodash';
 import parseFn from './parsers.js';
+import { isObject } from './utils.js';
+import formatter from './formatters/index.js';
 
 const readFile = (filePath) => {
   const currDir = cwd();
@@ -34,9 +36,6 @@ const getSortedUnionKeys = (obj1, obj2) => {
   return _.sortBy(_.union(obj1Keys, obj2Keys));
 };
 
-const isObject = (obj) =>
-  Object.prototype.toString.call(obj) === '[object Object]';
-
 const diffObjects = (obj1, obj2) => {
   const allSortedKeys = getSortedUnionKeys(obj1, obj2);
 
@@ -56,13 +55,13 @@ const diffObjects = (obj1, obj2) => {
       };
     } else if (!_.has(obj2, key)) {
       result = {
-        type: 'deleted',
+        type: 'removed',
         key,
         value: obj1[key],
       };
     } else if (!_.isEqual(obj1[key], obj2[key])) {
       result = {
-        type: 'changed',
+        type: 'updated',
         key,
         value: [obj1[key], obj2[key]],
       };
@@ -77,64 +76,7 @@ const diffObjects = (obj1, obj2) => {
   });
 };
 
-const makeReplaces = (depth, replacer = ' ', spacesCount = 2) =>
-  `${replacer.repeat(spacesCount * (2 * depth - 1))}`;
-
-const makeString = (data, depth) => {
-  if (!isObject(data)) {
-    return data;
-  }
-
-  const result = Object.entries(data).map(
-    ([key, value]) =>
-      `${makeReplaces(depth + 1)}  ${key}: ${makeString(value, depth + 1)}`
-  );
-  return `{\n${result.join('\n')}\n${makeReplaces(depth)}  }`;
-};
-
-const getString = (key, value, depth, symbol = ' ') =>
-  `${makeReplaces(depth)}${symbol} ${key}: ${makeString(value, depth)}`;
-
-const stylish = (data) => {
-  const iter = (node, depth) =>
-    node.map(({ type, key, value }) => {
-      let result;
-      switch (type) {
-        case 'nested':
-          result = [
-            `${makeReplaces(depth)}  ${key}: {`,
-            iter(value, depth + 1),
-            `${makeReplaces(depth)}  }`,
-          ];
-          break;
-        case 'added':
-          result = getString(key, value, depth, '+');
-          break;
-        case 'deleted':
-          result = getString(key, value, depth, '-');
-          break;
-        case 'changed':
-          result = `${getString(key, value[0], depth, '-')}\n${getString(
-            key,
-            value[1],
-            depth,
-            '+'
-          )}`;
-          break;
-        case 'unchanged':
-          result = getString(key, value, depth);
-          break;
-        default:
-          throw new Error(`This ${type} type is not found`);
-      }
-      return result;
-    });
-
-  const result = iter(data, 1).flat(Infinity);
-  return `{\n${result.join('\n')}\n}`;
-};
-
-const genDiff = (file1, file2, formatter = 'stylish') => {
+const genDiff = (file1, file2, formatName = 'stylish') => {
   const extname1 = getExtName(file1);
   const extname2 = getExtName(file2);
   const file1Content = getFileContent(file1, extname1);
@@ -142,9 +84,7 @@ const genDiff = (file1, file2, formatter = 'stylish') => {
   let result = '';
   if (extname1 === extname2 && file1Content && file2Content) {
     const tree = diffObjects(file1Content, file2Content);
-    if (formatter === 'stylish') {
-      result = stylish(tree);
-    }
+    result = formatter(tree, formatName);
   } else if (extname1 !== extname2) {
     result = 'Extnames are not equal';
   } else {
