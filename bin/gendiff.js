371e1413bb3ebeafa92a88ee3faa3922d002c634
Anton Liubimov <ant.liubimov@gmail.com>
2022-09-18 22:29:39 +0500

    fix: change option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,6 +10,8 @@ program
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .action((filepath1, filepath2, options) =>
+    console.log(genDiff(filepath1, filepath2, options.format))
+  );
 
 program.parse();
